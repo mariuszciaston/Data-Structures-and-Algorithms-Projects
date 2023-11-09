acaca2ff927b596c1bf11c31415c0f5f035634fc
Mariusz <casmar90@gmail.com>
2023-11-09 13:36:10 +0100

    ready

--- Project_Recursion/Project_Merge_sort.js
+++ Project_Recursion/Project_Merge_sort.js
@@ -1,93 +1,41 @@
-// const unsortedArray = [5, 2, 8, 1, 7, 3, 4, 6, 1];
+const unsortedArray = [5, 30, 9, 1, -2]; // [-2, 1, 5, 9, 30]
 
-// const unsortedArray = [5, 2, 8, 1, 7, 3, 4, 6];
+function merge(left, right) {
+	const sortedArray = [];
+	let indexLeft = 0;
+	let indexRight = 0;
 
-// const unsortedArray = [5, 2, 8, 1, 7, 3, 4];
+	while (indexLeft < left.length && indexRight < right.length) {
+		if (left[indexLeft] < right[indexRight]) {
+			sortedArray.push(left[indexLeft]);
+			indexLeft += 1;
+		} else {
+			sortedArray.push(right[indexRight]);
+			indexRight += 1;
+		}
+	}
 
-const unsortedArray = [5, 4, 3, 2, 1];
+	const remainingLeft = left.slice(indexLeft);
+	const remainingRight = right.slice(indexRight);
 
-// const unsortedArray = [5, 2, 1, 3, 6, 4];
+	return sortedArray.concat(remainingLeft).concat(remainingRight);
+}
 
-// sort left half of elements
-// sort right half of elements
-// merge sorted halves
-
-function mergeSort(array, sortedArray = []) {
+function mergeSort(array) {
 	if (array.length < 2) {
 		return array;
 	}
 
-	const middlePoint = Math.ceil(array.length / 2);
-	let leftHalf = array.slice(0, middlePoint);
-	let rightHalf = array.slice(middlePoint);
+	const middle = Math.ceil(array.length / 2);
+	let left = array.slice(0, middle);
+	let right = array.slice(middle);
+	left = mergeSort(left);
+	right = mergeSort(right);
 
-	console.log(`\u001b[1;35m left: ${leftHalf}`);
-	console.log(`\u001b[1;34m right: ${rightHalf}`);
-
-	mergeSort(leftHalf);
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-	// mergeSort(rightHalf);
-
-
-
-
-	// if (rightHalf.length === 1) {
-	// 	console.log(`RRRRRRRRRRRRRRR ${rightHalf}`);
-
-	// 	if (leftHalf > rightHalf) {
-	// 		const temp = rightHalf;
-	// 		rightHalf = leftHalf;
-	// 		leftHalf = temp;
-	// 	}
-	// }
-
-	// if (leftHalf.length === 1) {
-	// 	console.log(`LLLLLLLLLLLLLLL ${leftHalf}`);
-
-	// 	if (leftHalf > rightHalf) {
-	// 		const temp = rightHalf;
-	// 		rightHalf = leftHalf;
-	// 		leftHalf = temp;
-	// 	}
-
-
-
-	// 	sortedArray.push(leftHalf[0], rightHalf[0]);
-	// 	console.log(sortedArray);
-
-		// sortedArray.push(leftHalf, rightHalf);
-	// }
-
-
-
-
-
-
-	// const sortedArray = [];
-
-	// sortedArray.push(leftHalf, rightHalf);
-
-	// console.log( sortedArray);
-
-	// 	sortedArray.push(leftHalf, rightHalf);
-
-	// return sortedArray
-
-	// return sortedArray;
+	return merge(left, right);
 }
 
-console.log(`\u001b[1;31m ${mergeSort(unsortedArray)}`);
+
+
+
+console.log(mergeSort(unsortedArray));
