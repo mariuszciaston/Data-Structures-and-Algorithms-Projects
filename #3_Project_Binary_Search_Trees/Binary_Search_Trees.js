a25228af2e169867f2a939b145103a47c101f97a
Mariusz <casmar90@gmail.com>
2023-11-20 15:19:40 +0100

    Tree build work

--- #3_Project_Binary_Search_Trees/Binary_Search_Trees.js
+++ #3_Project_Binary_Search_Trees/Binary_Search_Trees.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-classes-per-file */
 
-// const arrayOfData = [1, 2, 3, 4, 5, 6, 7, 8];
-const arrayOfData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const arrayOfData = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// const arrayOfData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 function merge(left, right) {
 	const sortedArray = [];
@@ -48,3 +48,80 @@ const sortedArray = mergeSort(withoutDuplicates);
 console.log(`arrayOfData: ${arrayOfData}`);
 console.log(`withoutDuplicates: ${withoutDuplicates}`);
 console.log(`sortedArray: ${sortedArray}`);
+console.log('----------------------------------------');
+
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.leftChild = null;
+		this.rightChild = null;
+	}
+}
+
+// function to convert the array to BST and return the root of the created tree
+function buildTree(array) {
+	// if the array is empty return NULL
+	if (array.length === 0) {
+		return null;
+	}
+
+	let mid = Math.floor(array.length / 2);
+	const root = new Node(array[mid]);
+
+	// initializing queue
+	const queue = [
+		[root, [0, mid - 1]],
+		[root, [mid + 1, array.length - 1]],
+	];
+
+	while (queue.length > 0) {
+		const [parent, [leftChild, rightChild]] = queue.shift();
+
+		// if there are elements to process and parent node is not NULL
+		if (leftChild <= rightChild && parent != null) {
+			mid = Math.floor((leftChild + rightChild) / 2);
+			const child = new Node(array[mid]);
+
+			// set the child node as left or right child of the parent node
+			if (array[mid] < parent.data) {
+				parent.leftChild = child;
+			} else {
+				parent.rightChild = child;
+			}
+
+			// push the left and right child and their indices to the queue
+			queue.push([child, [leftChild, mid - 1]]);
+			queue.push([child, [mid + 1, rightChild]]);
+		}
+	}
+
+	return root;
+}
+
+// function to print the preorder traversal of the constructed BST
+function printBST(root) {
+	if (root === null) {
+		return;
+	}
+	console.log(`${root.data} `);
+	printBST(root.leftChild);
+	printBST(root.rightChild);
+}
+
+// function to print the whole BST visually
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+	if (node === null) {
+		return;
+	}
+	if (node.rightChild !== null) {
+		prettyPrint(node.rightChild, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+	}
+	console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+	if (node.leftChild !== null) {
+		prettyPrint(node.leftChild, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+	}
+};
+
+printBST(buildTree(sortedArray));
+console.log('----------------------------------------');
+prettyPrint(buildTree(sortedArray));
