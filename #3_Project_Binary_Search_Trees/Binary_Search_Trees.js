fdbb926135475a4625585f9fed20c9960660f28f
Mariusz <casmar90@gmail.com>
2023-11-20 18:13:05 +0100

    Add more methods

--- #3_Project_Binary_Search_Trees/Binary_Search_Trees.js
+++ #3_Project_Binary_Search_Trees/Binary_Search_Trees.js
@@ -1,127 +1,330 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable max-classes-per-file */
 
-const arrayOfData = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-// const arrayOfData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+class Node {
+	constructor(data, left = null, right = null, parent = null) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
 
-function merge(left, right) {
-	const sortedArray = [];
-	let indexLeft = 0;
-	let indexRight = 0;
+	height() {
+		if (this === null) {
+			return -1;
+		}
 
-	while (indexLeft < left.length && indexRight < right.length) {
-		if (left[indexLeft] < right[indexRight]) {
-			sortedArray.push(left[indexLeft]);
-			indexLeft += 1;
+		const leftHeight = this.left ? this.left.height() : -1;
+		const rightHeight = this.right ? this.right.height() : -1;
+
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+	depth() {
+		let depth = 0;
+		let node = this;
+		while (node.parent !== null) {
+			depth += 1;
+			node = node.parent;
+		}
+		return depth;
+	}
+}
+
+class Tree {
+	constructor(dataArray) {
+		this.root = this.prepare(dataArray);
+	}
+
+	prepare(dataArray) {
+		// Remove duplicates and sort the array
+		const sortedArray = [...new Set(dataArray)].sort((a, b) => a - b);
+		return this.buildTree(sortedArray, 0, sortedArray.length - 1);
+	}
+
+	// buildTree(sortedArray, start, end) {
+	// 	// Base case
+	// 	if (start > end) {
+	// 		return null;
+	// 	}
+
+	// 	// Find the middle and make it root
+	// 	const mid = Math.floor((start + end) / 2);
+	// 	const node = new Node(sortedArray[mid]);
+
+	// 	// Construct the left and right subtrees
+	// 	node.left = this.buildTree(sortedArray, start, mid - 1);
+	// 	node.right = this.buildTree(sortedArray, mid + 1, end);
+
+	// 	return node;
+	// }
+
+	buildTree(sortedArray, start, end, parent = null) {
+		// Base case
+		if (start > end) {
+			return null;
+		}
+
+		// Find the middle and make it root
+		const mid = Math.floor((start + end) / 2);
+		const node = new Node(sortedArray[mid]);
+
+		// Set parent
+		node.parent = parent;
+
+		// Construct the left and right subtrees
+		node.left = this.buildTree(sortedArray, start, mid - 1, node);
+		node.right = this.buildTree(sortedArray, mid + 1, end, node);
+
+		return node;
+	}
+
+	prettyPrint = (node = this.root, prefix = '', isLeft = true) => {
+		if (node === null) {
+			return;
+		}
+		if (node.right !== null) {
+			this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+		}
+		console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+		if (node.left !== null) {
+			this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+		}
+	};
+
+	// -------------------------------------------------------------------------------------
+
+	insert(data) {
+		const newNode = new Node(data);
+		if (this.root === null) {
+			this.root = newNode;
 		} else {
-			sortedArray.push(right[indexRight]);
-			indexRight += 1;
+			this.insertNode(this.root, newNode);
 		}
 	}
 
-	const remainingLeft = left.slice(indexLeft);
-	const remainingRight = right.slice(indexRight);
-
-	return sortedArray.concat(remainingLeft).concat(remainingRight);
-}
-
-function mergeSort(array) {
-	if (array.length < 2) {
-		return array;
+	insertNode(node, newNode) {
+		if (newNode.data < node.data) {
+			if (node.left === null) {
+				node.left = newNode;
+				newNode.parent = node;
+			} else {
+				this.insertNode(node.left, newNode);
+			}
+		} else if (node.right === null) {
+			node.right = newNode;
+			newNode.parent = node;
+		} else {
+			this.insertNode(node.right, newNode);
+		}
 	}
 
-	const middle = Math.ceil(array.length / 2);
-	let left = array.slice(0, middle);
-	let right = array.slice(middle);
-	left = mergeSort(left);
-	right = mergeSort(right);
-
-	return merge(left, right);
-}
-
-function removeDuplicates(data) {
-	return Array.from(new Set(data));
-}
-
-const withoutDuplicates = removeDuplicates(arrayOfData);
-const sortedArray = mergeSort(withoutDuplicates);
-
-console.log(`arrayOfData: ${arrayOfData}`);
-console.log(`withoutDuplicates: ${withoutDuplicates}`);
-console.log(`sortedArray: ${sortedArray}`);
-console.log('----------------------------------------');
-
-class Node {
-	constructor(data) {
-		this.data = data;
-		this.leftChild = null;
-		this.rightChild = null;
+	delete(data) {
+		this.root = this.deleteNode(this.root, data);
 	}
-}
 
-// function to convert the array to BST and return the root of the created tree
-function buildTree(array) {
-	// if the array is empty return NULL
-	if (array.length === 0) {
+	deleteNode(node, key) {
+		if (node === null) {
+			return null;
+		}
+
+		if (key < node.data) {
+			node.left = this.deleteNode(node.left, key);
+			return node;
+		}
+		if (key > node.data) {
+			node.right = this.deleteNode(node.right, key);
+			return node;
+		}
+		if (node.left === null && node.right === null) {
+			node = null;
+			return node;
+		}
+		if (node.left === null) {
+			node = node.right;
+			return node;
+		}
+		if (node.right === null) {
+			node = node.left;
+			return node;
+		}
+
+		const aux = this.findMinNode(node.right);
+		node.data = aux.data;
+
+		node.right = this.deleteNode(node.right, aux.data);
+		return node;
+	}
+
+	findMinNode(node) {
+		if (node.left === null) {
+			return node;
+		}
+		return this.findMinNode(node.left);
+	}
+
+	// -------------------------------------------------------------------------------------
+
+	find(data) {
+		let current = this.root;
+		while (current) {
+			if (data === current.data) {
+				// console.log(`Found node with data: ${current.data}`);
+				return current;
+			}
+			if (data < current.data) {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+		}
+		// console.log('Data not found in tree');
 		return null;
 	}
 
-	let mid = Math.floor(array.length / 2);
-	const root = new Node(array[mid]);
+	levelOrder(callback = (node) => node.data) {
+		const queue = [this.root];
+		const result = [];
 
-	// initializing queue
-	const queue = [
-		[root, [0, mid - 1]],
-		[root, [mid + 1, array.length - 1]],
-	];
+		while (queue.length > 0) {
+			const node = queue.shift();
+			result.push(callback(node));
 
-	while (queue.length > 0) {
-		const [parent, [leftChild, rightChild]] = queue.shift();
-
-		// if there are elements to process and parent node is not NULL
-		if (leftChild <= rightChild && parent != null) {
-			mid = Math.floor((leftChild + rightChild) / 2);
-			const child = new Node(array[mid]);
-
-			// set the child node as left or right child of the parent node
-			if (array[mid] < parent.data) {
-				parent.leftChild = child;
-			} else {
-				parent.rightChild = child;
+			if (node.left !== null) {
+				queue.push(node.left);
 			}
+			if (node.right !== null) {
+				queue.push(node.right);
+			}
+		}
 
-			// push the left and right child and their indices to the queue
-			queue.push([child, [leftChild, mid - 1]]);
-			queue.push([child, [mid + 1, rightChild]]);
+		return result;
+	}
+
+	inOrder(callback = (node) => node.data) {
+		const result = [];
+		this.inOrderTraverse(this.root, callback, result);
+		return result;
+	}
+
+	inOrderTraverse(node, callback, result) {
+		if (node !== null) {
+			this.inOrderTraverse(node.left, callback, result);
+			result.push(callback(node));
+			this.inOrderTraverse(node.right, callback, result);
 		}
 	}
 
-	return root;
+	preOrder(callback = (node) => node.data) {
+		const result = [];
+		this.preOrderTraverse(this.root, callback, result);
+		return result;
+	}
+
+	preOrderTraverse(node, callback, result) {
+		if (node !== null) {
+			result.push(callback(node));
+			this.preOrderTraverse(node.left, callback, result);
+			this.preOrderTraverse(node.right, callback, result);
+		}
+	}
+
+	postOrder(callback = (node) => node.data) {
+		const result = [];
+		this.postOrderTraverse(this.root, callback, result);
+		return result;
+	}
+
+	postOrderTraverse(node, callback, result) {
+		if (node !== null) {
+			this.postOrderTraverse(node.left, callback, result);
+			this.postOrderTraverse(node.right, callback, result);
+			result.push(callback(node));
+		}
+	}
+
+	isBalanced(node = this.root) {
+		if (node === null) {
+			return true;
+		}
+
+		const leftHeight = node.left ? node.left.height() : -1;
+		const rightHeight = node.right ? node.right.height() : -1;
+
+		if (Math.abs(leftHeight - rightHeight) > 1) {
+			return false;
+		}
+
+		return this.isBalanced(node.left) && this.isBalanced(node.right);
+	}
+
+	rebalance() {
+		const nodes = this.inOrder();
+		this.root = this.buildTree(nodes, 0, nodes.length - 1);
+	}
 }
 
-// function to print the preorder traversal of the constructed BST
-function printBST(root) {
-	if (root === null) {
-		return;
-	}
-	console.log(`${root.data} `);
-	printBST(root.leftChild);
-	printBST(root.rightChild);
-}
+// Test
+const tree = new Tree([1, 2, 3, 4, 5, 6, 7, 8]);
+// const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-// function to print the whole BST visually
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-	if (node === null) {
-		return;
-	}
-	if (node.rightChild !== null) {
-		prettyPrint(node.rightChild, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-	}
-	console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-	if (node.leftChild !== null) {
-		prettyPrint(node.leftChild, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-	}
-};
+tree.insert(69);
+tree.insert(9);
+tree.insert(77);
 
-printBST(buildTree(sortedArray));
+tree.delete(69);
+
+tree.find(4);
+
+const levelOrderResult = tree.levelOrder();
+
+// prints the data of all nodes in level order
+console.log(`levelOrderResult: ${levelOrderResult}`);
+
+// prints the data of all nodes in level order
+// tree.levelOrder((node) => console.log(node.data));
+
 console.log('----------------------------------------');
-prettyPrint(buildTree(sortedArray));
+
+const inOrderResult = tree.inOrder();
+console.log(`inOrderResult: ${inOrderResult}`); // prints the data of all nodes in in-order
+
+const preOrderResult = tree.preOrder();
+console.log(`preOrderResult: ${preOrderResult}`); // prints the data of all nodes in pre-order
+
+const postOrderResult = tree.postOrder();
+console.log(`postOrderResult: ${postOrderResult}`); // prints the data of all nodes in post-order
+
+// tree.inOrder((node) => console.log(node.data));  // prints the data of all nodes in in-order
+// tree.preOrder((node) => console.log(node.data));  // prints the data of all nodes in pre-order
+// tree.postOrder((node) => console.log(node.data));  // prints the data of all nodes in post-order
+
+console.log('----------------------------------------');
+
+const height = tree.root.height();
+
+// prints the height of the root node
+console.log(`Height of the root node: ${height}`);
+
+console.log('----------------------------------------');
+
+const node = tree.find(77);
+if (node !== null) {
+	const depth = node.depth();
+	console.log(`Depth of node with data 77: ${depth}`);
+} else {
+	console.log('Data not found in tree');
+}
+
+console.log('----------------------------------------');
+
+// prints true if the tree is balanced, false otherwise
+console.log(tree.isBalanced());
+
+tree.rebalance();
+
+console.log(tree.isBalanced());
+
+console.log('----------------------------------------');
+
+console.log('BINARY SEARCH TREE: ');
+tree.prettyPrint();
