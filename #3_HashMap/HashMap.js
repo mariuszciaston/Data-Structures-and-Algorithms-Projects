8b38dd28f6a305c1b2db98c3770d1ba4d8d96b3b
Mariusz <casmar90@gmail.com>
2024-07-31 21:03:27 +0200

    Add resize & checkIndexBounds methods

--- #3_HashMap/HashMap.js
+++ #3_HashMap/HashMap.js
@@ -1,8 +1,43 @@
 class HashMap {
 	constructor(capacity = 16, loadFactor = 0.75) {
 		this.capacity = capacity;
+		this.loadFactor = loadFactor;
 		this.buckets = new Array(this.capacity).fill(null);
 		this.threshold = capacity * loadFactor;
+	}
+
+	resize() {
+		if (this.length() >= this.threshold) {
+			const newCapacity = this.capacity * 2;
+			const newBuckets = new Array(newCapacity).fill(null);
+
+			this.capacity = newCapacity;
+
+			this.buckets.forEach((bucket) => {
+				if (bucket !== null) {
+					const index = this.hash(bucket.key);
+					newBuckets[index] = bucket;
+				}
+			});
+
+			this.buckets = newBuckets;
+			this.threshold = newCapacity * this.loadFactor;
+		} else if (this.length() < this.threshold) {
+			const newCapacity = Math.max(16, Math.floor(this.capacity / 2));
+			const newBuckets = new Array(newCapacity).fill(null);
+
+			this.capacity = newCapacity;
+
+			this.buckets.forEach((bucket) => {
+				if (bucket !== null) {
+					const index = this.hash(bucket.key);
+					newBuckets[index] = bucket;
+				}
+			});
+
+			this.buckets = newBuckets;
+			this.threshold = newCapacity * this.loadFactor;
+		}
 	}
 
 	hash(key) {
@@ -16,37 +51,37 @@ class HashMap {
 		return hashCode;
 	}
 
-	set(key, value) {
-		const index = this.hash(key);
+	checkIndexBounds(index) {
 		if (index < 0 || index >= this.buckets.length) {
 			throw new Error('Trying to access index out of bound');
 		}
+	}
+
+	set(key, value) {
+		const index = this.hash(key);
+		this.checkIndexBounds(index);
 		this.buckets[index] = { key, value };
+		this.resize();
 	}
 
 	get(key) {
 		const index = this.hash(key);
-		if (index < 0 || index >= this.buckets.length) {
-			throw new Error('Trying to access index out of bound');
-		}
+		this.checkIndexBounds(index);
 		return this.buckets[index]?.value || null;
 	}
 
 	has(key) {
 		const index = this.hash(key);
-		if (index < 0 || index >= this.buckets.length) {
-			throw new Error('Trying to access index out of bound');
-		}
+		this.checkIndexBounds(index);
 		return this.buckets[index]?.key === key;
 	}
 
 	remove(key) {
 		const index = this.hash(key);
-		if (index < 0 || index >= this.buckets.length) {
-			throw new Error('Trying to access index out of bound');
-		}
+		this.checkIndexBounds(index);
 		if (this.buckets[index]?.key === key) {
 			this.buckets[index] = null;
+			this.resize();
 			return true;
 		}
 		return false;
@@ -58,6 +93,7 @@ class HashMap {
 
 	clear() {
 		this.buckets.fill(null);
+		this.resize();
 		return true;
 	}
 
@@ -89,16 +125,29 @@ test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
 
-console.log(test);
+test.set('moon', 'silver');
 
-console.log('Length:', test.length());
-console.log('Get:', test.get('apple'));
-console.log('Has:', test.has('apple'));
-console.log('Remove:', test.remove('apple'));
-console.log('Has:', test.has('apple'));
-console.log('Length:', test.length());
+test.set('A', '1');
+test.set('B', '2');
+test.set('C', '3');
+test.set('D', '4');
+test.set('E', '5');
+
+test.set('F', '6');
+
+// console.log('Length:', test.length());
+// console.log('Get:', test.get('apple'));
+// console.log('Has:', test.has('apple'));
+// console.log('Remove:', test.remove('apple'));
+// console.log('Has:', test.has('apple'));
+// console.log('Length:', test.length());
 // console.log("Clear:", test.clear());
 // console.log("Length:", test.length());
-console.log('Keys:', test.keys());
-console.log('Values:', test.values());
-console.log('Entries:', test.entries());
+// console.log('Keys:', test.keys());
+// console.log('Values:', test.values());
+// console.log('Entries:', test.entries());
+
+console.log(test.buckets);
+console.log('Length:', test.length());
+console.log('Threshold:', test.threshold);
+console.log('Capacity:', test.capacity);
