49346538b2f73d529604d87abd6dc2025575cbf5
Mariusz <casmar90@gmail.com>
2024-08-02 17:32:05 +0200

    Add LinkedLists to buckets and update HashMap methods

--- #3_HashMap/HashMap-class.js
+++ #3_HashMap/HashMap-class.js
@@ -1,4 +1,4 @@
-/* eslint max-classes-per-file: "off" */
+/* eslint-disable max-classes-per-file */
 
 class Node {
 	constructor(value = null, nextNode = null) {
@@ -7,8 +7,26 @@ class Node {
 	}
 }
 
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
 
+	append(value) {
+		const newNode = new Node(value);
+		this.size += 1;
 
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.nextNode = newNode;
+			this.tail = newNode;
+		}
+	}
+}
 
 class HashMap {
 	constructor(capacity = 16, loadFactor = 0.75) {
@@ -18,39 +36,37 @@ class HashMap {
 		this.threshold = capacity * loadFactor;
 	}
 
-	resize() {
-		if (this.length() >= this.threshold) {
-			const newCapacity = this.capacity * 2;
-			const newBuckets = new Array(newCapacity).fill(null);
+	// resize() {
+	// 	if (this.length() >= this.threshold) {
+	// 		const newCapacity = this.capacity * 2;
+	// 		const newBuckets = new Array(newCapacity).fill(null);
+	// 		this.capacity = newCapacity;
 
-			this.capacity = newCapacity;
+	// 		this.buckets.forEach((bucket) => {
+	// 			if (bucket !== null) {
+	// 				const index = this.hash(bucket.head.value.key);
+	// 				newBuckets[index] = bucket;
+	// 			}
+	// 		});
 
-			this.buckets.forEach((bucket) => {
-				if (bucket !== null) {
-					const index = this.hash(bucket.key);
-					newBuckets[index] = bucket;
-				}
-			});
+	// 		this.buckets = newBuckets;
+	// 		this.threshold = newCapacity * this.loadFactor;
+	// 	} else if (this.length() < this.threshold) {
+	// 		const newCapacity = Math.max(16, Math.floor(this.capacity / 2));
+	// 		const newBuckets = new Array(newCapacity).fill(null);
+	// 		this.capacity = newCapacity;
 
-			this.buckets = newBuckets;
-			this.threshold = newCapacity * this.loadFactor;
-		} else if (this.length() < this.threshold) {
-			const newCapacity = Math.max(16, Math.floor(this.capacity / 2));
-			const newBuckets = new Array(newCapacity).fill(null);
+	// 		this.buckets.forEach((bucket) => {
+	// 			if (bucket !== null) {
+	// 				const index = this.hash(bucket.head.value.key);
+	// 				newBuckets[index] = bucket;
+	// 			}
+	// 		});
 
-			this.capacity = newCapacity;
-
-			this.buckets.forEach((bucket) => {
-				if (bucket !== null) {
-					const index = this.hash(bucket.key);
-					newBuckets[index] = bucket;
-				}
-			});
-
-			this.buckets = newBuckets;
-			this.threshold = newCapacity * this.loadFactor;
-		}
-	}
+	// 		this.buckets = newBuckets;
+	// 		this.threshold = newCapacity * this.loadFactor;
+	// 	}
+	// }
 
 	hash(key) {
 		let hashCode = 0;
@@ -72,28 +88,39 @@ class HashMap {
 	set(key, value) {
 		const index = this.hash(key);
 		this.checkIndexBounds(index);
-		this.buckets[index] = { key, value };
-		this.resize();
+
+		if (this.buckets[index] === null) {
+			this.buckets[index] = new LinkedList();
+		}
+
+		if (this.buckets[index].head?.value.key === key) {
+			this.buckets[index].head.value.value = value;
+		} else {
+			this.buckets[index].append({ key, value });
+		}
+		// this.resize();
 	}
 
 	get(key) {
 		const index = this.hash(key);
 		this.checkIndexBounds(index);
-		return this.buckets[index]?.value || null;
+		return this.buckets[index]?.head.value.value || null;
 	}
 
 	has(key) {
 		const index = this.hash(key);
 		this.checkIndexBounds(index);
-		return this.buckets[index]?.key === key;
+		return this.buckets[index]?.head.value.key === key;
 	}
 
 	remove(key) {
+		// Remove method needs to be fixed
 		const index = this.hash(key);
 		this.checkIndexBounds(index);
-		if (this.buckets[index]?.key === key) {
+
+		if (this.buckets[index]?.head.value.key === key) {
 			this.buckets[index] = null;
-			this.resize();
+			// this.resize();
 			return true;
 		}
 		return false;
@@ -105,20 +132,50 @@ class HashMap {
 
 	clear() {
 		this.buckets.fill(null);
-		this.resize();
+		// this.resize();
 		return true;
 	}
 
 	keys() {
-		return this.buckets.filter((bucket) => bucket !== null).map((bucket) => bucket.key);
+		const result = [];
+
+		this.buckets.forEach((bucket) => {
+			let currentNode = bucket?.head;
+			while (currentNode) {
+				result.push(currentNode.value.key);
+				currentNode = currentNode.nextNode;
+			}
+		});
+
+		return result;
 	}
 
 	values() {
-		return this.buckets.filter((bucket) => bucket !== null).map((bucket) => bucket.value);
+		const result = [];
+
+		this.buckets.forEach((bucket) => {
+			let currentNode = bucket?.head;
+			while (currentNode) {
+				result.push(currentNode.value.value);
+				currentNode = currentNode.nextNode;
+			}
+		});
+
+		return result;
 	}
 
 	entries() {
-		return this.buckets.filter((bucket) => bucket !== null).map((bucket) => [bucket.key, bucket.value]);
+		const result = [];
+
+		this.buckets.forEach((bucket) => {
+			let currentNode = bucket?.head;
+			while (currentNode) {
+				result.push([currentNode.value.key, currentNode.value.value]);
+				currentNode = currentNode.nextNode;
+			}
+		});
+
+		return result;
 	}
 }
 
@@ -136,30 +193,30 @@ test.set('ice cream', 'white');
 test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
-
 test.set('moon', 'silver');
-
 test.set('A', '1');
 test.set('B', '2');
 test.set('C', '3');
 test.set('D', '4');
 test.set('E', '5');
 
-test.set('F', '6');
+// test.set('F', '6');
 
+console.log('Length:', test.length());
+console.log('Get:', test.get('apple'));
+console.log('Has:', test.has('apple'));
+// console.log('Remove:', test.remove('elephant'));
+console.log('Has:', test.has('apple'));
+console.log('Length:', test.length());
+// console.log('Clear:', test.clear());
 // console.log('Length:', test.length());
-// console.log('Get:', test.get('apple'));
-// console.log('Has:', test.has('apple'));
-// console.log('Remove:', test.remove('apple'));
-// console.log('Has:', test.has('apple'));
-// console.log('Length:', test.length());
-// console.log("Clear:", test.clear());
-// console.log("Length:", test.length());
-// console.log('Keys:', test.keys());
-// console.log('Values:', test.values());
-// console.log('Entries:', test.entries());
+console.log('Keys:', test.keys());
+console.log('Values:', test.values());
+console.log('Entries:', test.entries());
 
 console.log(test.buckets);
+
+console.log('-----------------');
 console.log('Length:', test.length());
 console.log('Threshold:', test.threshold);
 console.log('Capacity:', test.capacity);
